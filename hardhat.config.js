abcce0471ac15d4ef078c96e79b06bfb88debb00
natangomesufrj <140290126+natangomesufrj@users.noreply.github.com>
2025-07-07 19:56:06 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+  solidity: "0.8.28",
+
 };
